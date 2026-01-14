90948079199b981f87484894172ddbf7a9b2fc46
cvm1912 <rajshivam1912@gmail.com>
2026-01-14 21:42:56 +0530

    express added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import express from 'express'
 import { PrismaClient } from "@prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 import pg, { Client } from "pg"
@@ -7,6 +8,70 @@ import { todo } from "node:test"
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
 const client = new PrismaClient({ adapter })
+
+
+const app = express()
+
+app.get('/users',(req,res)=>{
+    const users = client.user.findMany();
+    return res.status(200).json({
+        success:true,
+        message:"Data Retrived Successful",
+        data: users
+    })
+})
+
+
+app.get('/users/:id',(req,res)=>{
+   const userId = req.params.id
+
+   if(!userId){
+    return res.status(400).json({
+        success:false,
+        message:"User Id is required"
+    })
+   }
+
+   const result = client.user.findUnique({
+    where:{
+        id: parseInt(userId)
+    }
+   }) 
+
+   return res.status(200).json({
+        success:true,
+        message:"Data Retrived Successful",
+        data: result
+    })
+})
+
+
+app.post('/create', (req,res)=>{
+    const {username, password, firstName, lastName} = req.body
+    if(!username || !password || !firstName || !lastName){
+        return res.status(400).json({
+            success:false,
+            message:"All fields are required"
+        })
+    }
+
+    const result = client.user.create({data:{
+        username,
+        password,
+        firstName,
+        lastName
+    }})
+
+    return res.status(200).json({
+        success:true,
+        message:"User Created Successfully",
+        data: result
+    })
+})
+
+
+
+
 
 async function main() {
   await client.user.create({
@@ -47,7 +112,7 @@ async function getUser()
     console.log(result)
 }
 
-getUser();
+// getUser();
 
 
 async function updateUser(){
@@ -65,6 +130,53 @@ async function updateUser(){
 
 // updateUser();
 
+app.put('/update-user/:id',(req,res)=>{
+   let userId = req.params.id
+   if(!userId){
+    return res.json(        {
+            success:false,
+            message:"User id is required"
+        })
+   }
+
+   const result = client.user.findUnique({
+       where: {
+           id: parseInt(userId)
+       }
+   })
+
+   return res.status(200).json({
+       success:true,
+       message:"User Updated Successfully",
+       data: result
+   })
+})
+
+
+
+app.delete('delete-user/:id',(req,res)=>{
+   let userId = req.params.id
+   if(!userId){
+    return res.json(        {
+            success:false,
+            message:"User id is required"
+        })
+   }
+
+    const result = client.user.delete({
+       where: {
+           id: parseInt(userId)
+       }
+   })
+
+    return res.status(200).json({
+       success:true,
+       message:"User Deleted Successfully",
+       data: result
+   })
+
+})
+
 
 async function deleteUser(){
     let result = await client.user.delete({
@@ -76,4 +188,6 @@ async function deleteUser(){
     console.log("result is deleted")
 }
   
-
+app.listen(8000, ()=>{
+    console.log("Server is running on port 8000")
+})
